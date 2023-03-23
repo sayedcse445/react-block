4d5c3ad2b998b6972391ea3d339af85d321e205f
sayedcse445 <70788935+sayedcse445@users.noreply.github.com>
2023-03-24 01:51:36 +0600

    new

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import Header from "../../header/Header";
 import "./Home.css";
+import Posts from "../../posts/posts"
+import Sidebar from "../../sidebar/sidebar";
 
 export default function Home(){
     return(
+        <>
+        <Header/>
         <div className="home">
-            <Header/>
-            homepage
+        <Posts />
+        <Sidebar />
         </div>
+        </>
     )
 }
---
+++ my-app/src/posts/posts.jsx
@@ -0,0 +1,9 @@
+import "../posts/posts.css"
+
+export default function posts(){
+    return (
+        <div className="post">
+            posts
+        </div>
+    )
+}
